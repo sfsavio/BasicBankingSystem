7e48c19923307d1dc365ed18e74df69d24539f8f
Sávio Fontana <fontana.savio2002@gmail.com>
2024-09-24 13:05:00 -0300

    fix no getline

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include<vector>
 #include<string>
 #include<locale>
+#include<ios> //used to get stream size
+#include<limits> //used to get numeric limits
 
 std::string checkBook(std::vector<std::string> books, std::vector<std::string> status);
 std::string createBook(std::vector<std::string> books, std::string bookName, std::vector<std::string> status);
+std::string readBookName();
 //std::string rentBook(std::vector<std::string> books);
 //std::string returnBook(std::vector<std::string> books);
 
@@ -13,7 +16,7 @@ int main() {
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
     std::cout << "Bem vindo a Biblioteca Do IFMG - Por Favor Selecione Uma Opção:\n1. Cadastrar Livro \n2. Consultar Livro\n3. Alugar Livro\n4. Devolver Livro\n5. Sair";
-    
+    std::cout << std::endl;
     std::vector<std::string> books; 
     std::vector<std::string> status;
         
@@ -21,6 +24,7 @@ int main() {
     //std::cout << books.size();
     int op;
     std::cin>>op;
+    std::cin.ignore();
     switch(op) {
         case 1:
               checkBook(books, status);   
@@ -29,27 +33,37 @@ int main() {
               std::cout<<"default";
         break;
     }
+
+    
     
    // checkBook(books, status); 
 
     return 0;
 }
 std::string checkBook(std::vector<std::string> books, std::vector<std::string> status) {
-    std::string bookName;
-    std::cout << "\nDigite o nome livro para verificar se ele ja existe no sistema: \n";
-    std::getline(std::cin, bookName);
-
-    for (int i = 0; i < books.size(); i++)
-    {
-        if (bookName == books[i]){
-            std::cout << "Book Found In Position: " << i << std::endl;
-            std::cout << status[i];
-            checkBook(books, status);
-        } else {
-            
-        }
+    std::string bookName = readBookName();
+     for (int i = 0; i < books.size(); i++) {
+         if (bookName == books[i]) {
+             std::cout << "Book Found In Position: " << i << std::endl;
+             std::cout << status[i];
+             checkBook(books, status);
+         }
+     }
+    
+    char validate;
+    //system("cls");
+    std::cout << "Livro não cadastrado..\n";
+    std::cout << "Deseja cadastrar \"" << bookName << "\" ? (s/n)";
+    std::cin >> validate;
+    
+    if (validate == 's') {
+        createBook(books, bookName, status);
     }
-    createBook(books, bookName, status);    
+    if (validate == 'n') {
+        main();
+    }
+   
+    
 }
 
 std::string createBook(std::vector<std::string> books, std::string bookName, std::vector<std::string> status) {
@@ -65,6 +79,7 @@ std::string createBook(std::vector<std::string> books, std::string bookName, std
     }
     std::string op;
     std::cout << "Deseja adcionar um novo livro? (sim / nao) \n";
+    std::cin.ignore();
     std::getline(std::cin, op);
 
     while (true) {
@@ -73,10 +88,18 @@ std::string createBook(std::vector<std::string> books, std::string bookName, std
         }
         else {
             std::cout << "\n\nsaindo..";
-            break;
+            //main();
         }
         break;
     }
     /*checkBook(books, status);*/
   
+}
+
+std::string readBookName() {
+    std::string bookName;
+    std::cout << "\nDigite o nome livro para verificar se ele ja existe no sistema: \n";
+    std::getline(std::cin, bookName);
+ 
+    return bookName;
 }
